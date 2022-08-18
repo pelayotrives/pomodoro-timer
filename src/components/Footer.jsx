554d230c50884c6720482e54f04dfb8f1044dd08
Pelayo Trives <pelayotrives@gmail.com>
2022-08-18 16:55:16 +0200

    Footer updated.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,11 @@ import { UtilityContext } from '../context/global.context';
 
 export default function Footer() {
 
+  // Targetting the empty span.
+  const copyrightYear = document.querySelectorAll(".current-copyright-year")
+  // We assign the current year to the span.
+  copyrightYear.innerText = new Date().getFullYear();
+
   // De-structuring our context object.
   const {styles, toggleButton, isActive, setIsActive} = useContext(UtilityContext);
 
@@ -12,7 +17,8 @@ export default function Footer() {
         <footer className='fixed bottom-0 left-0 w-screen h-14 p-3 font-onlybody font-regular text-white tracking-wide'>{isActive === true &&
             " Dark Mode | "
           } Made with 🤍 by&nbsp;
-          <a className='underline' href="https://www.linkedin.com/in/pelayo-trives-pozuelo/">Pelayo Trives </a>
+          <a className='underline' href="https://www.linkedin.com/in/pelayo-trives-pozuelo/">Pelayo Trives</a> - 
+              <span className="current-copyright-year">&nbsp;{copyrightYear.innerText}</span>&nbsp;©
         </footer>
     </div>
   )
